9a4f1da8401fbe8fa99f60898c146ca45e62becf
TimoA1402 <149794142+TimoA1402@users.noreply.github.com>
2023-11-10 14:00:47 +0100

    Code ergänzt

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,47 +1,124 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <iostream>
+#include <vector>
 
-#include "linkedlist.h"
-
-void printer(const std::string &name)
+class LinkedListNode
 {
-    fmt::print("Printer: {}\n", name);
+public:
+    std::string data;
+    LinkedListNode* next;
+
+    LinkedListNode(const std::string& value) : data(value), next(nullptr) {}
+
+    // Hier könnte ein Destruktor hinzugefügt werden, um den allokierten Speicher für jeden Knoten freizugeben
+    // ~LinkedListNode() {}
+};
+
+class LinkedList
+{
+private:
+    LinkedListNode* head;
+    LinkedListNode* tail;
+    std::size_t size; // Renamed from 'count' to 'size'
+
+public:
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~LinkedList()
+    {
+        // Implementiere den Destruktor, um den allokierten Speicher für jeden Knoten freizugeben
+        LinkedListNode* current = head;
+        while (current)
+        {
+            LinkedListNode* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
+    void insert_tail(LinkedListNode* newNode)
+    {
+        if (!head)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    void insert_head(LinkedListNode* newNode)
+    {
+        if (!head)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    std::size_t getSize() const // Renamed from 'size()' to 'getSize()' to avoid conflicts
+    {
+        return size;
+    }
+
+    void traverse(void (*visit)(const std::string&))
+    {
+        LinkedListNode* current = head;
+        while (current)
+        {
+            visit(current->data);
+            current = current->next;
+        }
+    }
+};
+
+void printer(const std::string& name)
+{
+    fmt::print("Drucker: {}\n", name);
 }
 
-auto main(int argc, char **argv) -> int
+int main(int argc, char** argv)
 {
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", "World");
+    fmt::print("Hallo, {}!\n", "Welt");
 
     const std::vector<std::string> names{"Hugo", "Franz", "Elisabeth", "Anton", "Gerhard", "Maria", "Hannelore"};
     const std::vector<std::string> names2{"Ulf", "Ole", "Sepp", "Norton", "Kai", "Uschi", "Gert"};
+
     {
-        LinkedList list;
+        LinkedList liste;
         fmt::print("------------------------------\n");
-        for (auto &name : names)
+        for (const auto& name : names)
         {
             auto elem = new LinkedListNode(name);
-            // elem->print();
-            list.insert_tail(elem);
+            liste.insert_tail(elem);
         }
-        list.traverse(printer);
+        liste.traverse(printer);
         fmt::print("------------------------------\n");
-        for (auto &name : names2)
+        for (const auto& name : names2)
         {
             auto elem = new LinkedListNode(name);
-            // elem->print();
-            list.insert_head(elem);
+            liste.insert_head(elem);
         }
         fmt::print("------------------------------\n");
-        list.traverse(printer);
+        liste.traverse(printer);
         fmt::print("------------------------------\n");
-        fmt::print("Elements in the list: {}\n", list.size());
+        fmt::print("Elemente in der Liste: {}\n", liste.getSize());
         fmt::print("------------------------------\n");
-        list.traverse(printer);
-    }         /* this calls the DTOR of the list*/
-    return 0; /* exit gracefully*/
+        liste.traverse(printer);
+    } // Der Destruktor der Liste wird hier aufgerufen
+
+    return 0;
+
 }
+
