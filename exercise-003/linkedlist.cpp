276890193f70c3eb18bf24e4d8d2b85b7ba8126b
TimoA1402 <149794142+TimoA1402@users.noreply.github.com>
2023-11-10 14:44:24 +0100

    Liste

--- exercise-003/linkedlist.cpp
+++ exercise-003/linkedlist.cpp
@@ -2,67 +2,123 @@
 
 LinkedList::~LinkedList()
 {
-  for (auto tmp = m_head; tmp != nullptr; tmp = tmp->pNext /* prepare the next in the row */) {
-    auto elem = tmp; /* save the element to be deleted */
-    delete elem;
+  // Freigabe aller Elemente in der Liste
+  while (m_head != nullptr) {
+    auto temp = m_head;
+    m_head = m_head->pNext;
+    delete temp;
   }
 }
 
 bool LinkedList::insert_tail(LinkedListNode *node)
 {
-  bool ret = false;
-  if (nullptr == node) {
-    return ret;
+  // Überprüfen Sie auf Null-Pointer
+  if (node == nullptr) {
+    return false;
   }
-  // insert your code here....
-  return ret;
+
+  // Fügen Sie das Element am Ende der Liste ein
+  if (m_tail == nullptr) {
+    m_head = m_tail = node;
+  } else {
+    m_tail->pNext = node;
+    m_tail = node;
+  }
+  return true;
 }
 
 bool LinkedList::insert_head(LinkedListNode *node)
 {
-  bool ret = false;
-  if (nullptr == node) {
-    return ret;
+  // Überprüfen Sie auf Null-Pointer
+  if (node == nullptr) {
+    return false;
   }
-  // insert your code here....
-  return ret;
+
+  // Fügen Sie das Element am Anfang der Liste ein
+  node->pNext = m_head;
+  m_head = node;
+  if (m_tail == nullptr) {
+    m_tail = node;
+  }
+  return true;
 }
 
 bool LinkedList::insert_after(LinkedListNode *loc, LinkedListNode *node)
 {
-  bool ret = false;
-  if ((nullptr == loc) || (nullptr == node)) {
-    return ret;
+  // Überprüfen Sie auf Null-Pointer
+  if ((loc == nullptr) || (node == nullptr)) {
+    return false;
   }
-  // insert your code here ....
-  return ret;
+
+  // Fügen Sie das Element nach dem 'loc'-Element ein
+  node->pNext = loc->pNext;
+  loc->pNext = node;
+  if (loc == m_tail) {
+    m_tail = node;
+  }
+  return true;
 }
 
 bool LinkedList::insert_before(LinkedListNode *loc, LinkedListNode *node)
 {
-  bool ret = false;
-  if ((nullptr == loc) || (nullptr == node)) {
-    return ret;
+  // Überprüfen Sie auf Null-Pointer
+  if ((loc == nullptr) || (node == nullptr)) {
+    return false;
   }
-  // Insert your code here....
-  return ret;
+
+  // Fügen Sie das Element vor dem 'loc'-Element ein
+  if (loc == m_head) {
+    insert_head(node);
+  } else {
+    auto tmp = m_head;
+    while (tmp->pNext != loc) {
+      tmp = tmp->pNext;
+    }
+    tmp->pNext = node;
+    node->pNext = loc;
+  }
+  return true;
 }
 
 bool LinkedList::remove(LinkedListNode *node)
 {
-  bool ret = false;
-  // insert your code here ...
-  return ret;
+  // Überprüfen Sie auf Null-Pointer
+  if (node == nullptr || m_head == nullptr) {
+    return false;
+  }
+
+  // Entfernen Sie das Element aus der Liste
+  if (node == m_head) {
+    m_head = node->pNext;
+    if (node == m_tail) {
+      m_tail = nullptr;
+    }
+    delete node;
+    return true;
+  } else {
+    auto tmp = m_head;
+    while (tmp->pNext != nullptr && tmp->pNext != node) {
+      tmp = tmp->pNext;
+    }
+    if (tmp->pNext == node) {
+      tmp->pNext = node->pNext;
+      if (node == m_tail) {
+        m_tail = tmp;
+      }
+      delete node;
+      return true;
+    }
+  }
+  return false;
 }
 
 size_t LinkedList::size()
 {
   size_t count = 0;
-  /* using a lambda to count objects in the list*/
+  // Verwenden Sie eine Lambda-Funktion, um Objekte in der Liste zu zählen
   traverse([&count](LinkedListNode *node) { count++; });
   return count;
 }
-
 
 void LinkedList::traverse(std::function<void(const std::string &)> func)
 {
@@ -71,6 +127,7 @@ void LinkedList::traverse(std::function<void(const std::string &)> func)
 
 void LinkedList::traverse(std::function<void(LinkedListNode *node)> func)
 {
+  // Durchlaufen Sie die Liste und rufen Sie die Operation auf
   for (auto tmp = m_head; tmp != nullptr; tmp = tmp->pNext) {
     func(tmp);
   }
--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,124 +1,47 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
-#include <iostream>
-#include <vector>
 
-class LinkedListNode
+#include "linkedlist.h"
+
+void printer(const std::string &name)
 {
-public:
-    std::string data;
-    LinkedListNode* next;
-
-    LinkedListNode(const std::string& value) : data(value), next(nullptr) {}
-
-    // Hier könnte ein Destruktor hinzugefügt werden, um den allokierten Speicher für jeden Knoten freizugeben
-    // ~LinkedListNode() {}
-};
-
-class LinkedList
-{
-private:
-    LinkedListNode* head;
-    LinkedListNode* tail;
-    std::size_t size; // Renamed from 'count' to 'size'
-
-public:
-    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
-
-    ~LinkedList()
-    {
-        // Implementiere den Destruktor, um den allokierten Speicher für jeden Knoten freizugeben
-        LinkedListNode* current = head;
-        while (current)
-        {
-            LinkedListNode* next = current->next;
-            delete current;
-            current = next;
-        }
-    }
-
-    void insert_tail(LinkedListNode* newNode)
-    {
-        if (!head)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = newNode;
-        }
-        size++;
-    }
-
-    void insert_head(LinkedListNode* newNode)
-    {
-        if (!head)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            newNode->next = head;
-            head = newNode;
-        }
-        size++;
-    }
-
-    std::size_t getSize() const // Renamed from 'size()' to 'getSize()' to avoid conflicts
-    {
-        return size;
-    }
-
-    void traverse(void (*visit)(const std::string&))
-    {
-        LinkedListNode* current = head;
-        while (current)
-        {
-            visit(current->data);
-            current = current->next;
-        }
-    }
-};
-
-void printer(const std::string& name)
-{
-    fmt::print("Drucker: {}\n", name);
+    fmt::print("Printer: {}\n", name);
 }
 
-int main(int argc, char** argv)
+auto main(int argc, char **argv) -> int
 {
-    fmt::print("Hallo, {}!\n", "Welt");
+    /**
+     * The {fmt} lib is a cross platform library for printing and formatting text
+     * it is much more convenient than std::cout and printf
+     * More info at https://fmt.dev/latest/api.html
+     */
+    fmt::print("Hello, {}!\n", "World");
 
     const std::vector<std::string> names{"Hugo", "Franz", "Elisabeth", "Anton", "Gerhard", "Maria", "Hannelore"};
     const std::vector<std::string> names2{"Ulf", "Ole", "Sepp", "Norton", "Kai", "Uschi", "Gert"};
-
     {
-        LinkedList liste;
+        LinkedList list;
         fmt::print("------------------------------\n");
-        for (const auto& name : names)
+        for (auto &name : names)
         {
             auto elem = new LinkedListNode(name);
-            liste.insert_tail(elem);
+            elem->print();
+            list.insert_tail(elem);
         }
-        liste.traverse(printer);
+        list.traverse(printer);
         fmt::print("------------------------------\n");
-        for (const auto& name : names2)
+        for (auto &name : names2)
         {
             auto elem = new LinkedListNode(name);
-            liste.insert_head(elem);
+            elem->print();
+            list.insert_head(elem);
         }
         fmt::print("------------------------------\n");
-        liste.traverse(printer);
+        list.traverse(printer);
         fmt::print("------------------------------\n");
-        fmt::print("Elemente in der Liste: {}\n", liste.getSize());
+        fmt::print("Elements in the list: {}\n", list.size());
         fmt::print("------------------------------\n");
-        liste.traverse(printer);
-    } // Der Destruktor der Liste wird hier aufgerufen
-
-    return 0;
-
+        list.traverse(printer);
+    }         /* this calls the DTOR of the list*/
+    return 0; /* exit gracefully*/
 }
-
