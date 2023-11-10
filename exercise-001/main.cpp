3bd9a7ef4947009f8f911f638b6dcbae2c8f32e4
TimoA1402 <149794142+TimoA1402@users.noreply.github.com>
2023-11-10 09:27:51 +0100

    .

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -6,15 +6,63 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
+// .bss segment
+int bss;
+int bss2;
+int data = 4711;
+int data2 = 5000;
+const int rodata = 42;
+const int rodata2 = 50;
+
+int function(int value) {
+    int data =4711;
+    return function(data+value);
+}
 
 auto main(int argc, char **argv) -> int
 {
+    int stack;
+    int stack2 = 14;
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    
-    fmt::print("Hello, {}!\n", "hello");
+
+    fmt::print("Value of variable bss {} address of variable bss {}\n",
+    bss,
+    fmt::ptr(&bss));
+
+    fmt::print("Value of variable bss2 {} address of variable bss2 {}\n",
+    bss2,
+    fmt::ptr(&bss2));
+
+    fmt::print("Value of variable data {} address of variable data {}\n",
+    data,
+    fmt::ptr(&data));
+
+    fmt::print("Value of variable data2 {} address of variable data2 {}\n",
+    data2,
+    fmt::ptr(&data2));
+
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n",
+    rodata,
+    fmt::ptr(&rodata));
+
+    fmt::print("Value of variable rodata2 {} address of variable rodata2 {}\n",
+    rodata2,
+    fmt::ptr(&rodata2));
+
+    fmt::print("Value of variable stack {} address of variable stack {}\n",
+    stack,
+    fmt::ptr(&stack));
+
+    fmt::print("Value of variable stack2 {} address of variable stack2 {}\n",
+    stack2,
+    fmt::ptr(&stack2));
+
+    fmt::print("\nHello, {}!\n", "Geile Sau");
+
+    //function(23);
     return 0; /* exit gracefully*/
 }
