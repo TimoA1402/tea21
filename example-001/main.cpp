3e456ca6045e80dacc61eca2e5b50aa0f3211f15
TimoA1402 <149794142+TimoA1402@users.noreply.github.com>
2023-12-07 09:31:45 +0100

    Aktualisieren

--- example-001/main.cpp
+++ example-001/main.cpp
@@ -1,11 +1,15 @@
+#include "CLI/CLI.hpp"
 #include <fmt/format.h>
-#include <ranges> // requires C++ 20 support!
 
-auto main(int argc, char **argv) -> int
-{
-    fmt::println("Amount of parameters provided: {}",argc);
-    for (auto i : std::views::iota(0, argc)) {
-        fmt::println("argv[{}][{}\\0]",i,argv[i]);
-    }
+int main(int argc, char **argv) {
+    CLI::App app{"App description"};
+
+    // Define options
+    int p = 0;
+    app.add_option("-p", p, "Parameter");
+
+    CLI11_PARSE(app, argc, argv);
+
+    fmt::println("Parameter value: {}",p);
     return 0;
 }
